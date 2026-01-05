a95854d19263f006b72bfe7147ad9e49d148da3e
ShineShao <xiaoshaoqq@gmail.com>
2026-01-05 23:16:58 +0800

    feat: update deps

--- packages/DatePicker/dist/style.js
+++ packages/DatePicker/dist/style.js
@@ -1,6 +1,6 @@
 /*
-* @ezuikit/control-date-picker v1.0.4
-* Copyright (c) 2026-01-01 Ezviz-OpenBiz
+* @ezuikit/control-date-picker v1.0.5
+* Copyright (c) 2026-01-05 Ezviz-OpenBiz
 * Released under the MIT License.
 */
 require("./style.css");
