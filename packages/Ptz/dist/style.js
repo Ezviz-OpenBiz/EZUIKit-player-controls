c4b34a7a418f541b0fff397e45d9b0c3449facba
ShineShao <xiaoshaoqq@gmail.com>
2026-05-03 22:55:47 +0800

    feat: update ptz

--- packages/Ptz/dist/style.js
+++ packages/Ptz/dist/style.js
@@ -1,6 +1,6 @@
 /*
-* @ezuikit/control-ptz v1.0.0-alpha.1
-* Copyright (c) 2026-03-15 Ezviz-OpenBiz
+* @ezuikit/control-ptz v1.0.0-beta.1
+* Copyright (c) 2026-05-03 Ezviz-OpenBiz
 * Released under the MIT License.
 */
 require("./style.css");
