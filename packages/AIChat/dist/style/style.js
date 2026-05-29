d5c3c2c445c96af438532c1c869d4313acaba1a1
wangwenwen <48354328+wangwenwen130@users.noreply.github.com>
2026-05-29 15:08:04 +0800

    [feat] 优化样式

--- packages/AIChat/dist/style/style.js
+++ packages/AIChat/dist/style/style.js
@@ -1,6 +1,6 @@
 /*
-* @ezuikit/control-aichat v0.1.0-beta.5
-* Copyright (c) 2026-05-28 Ezviz-OpenBiz
+* @ezuikit/control-aichat v0.1.0-beta.6
+* Copyright (c) 2026-05-29 Ezviz-OpenBiz
 * Released under the MIT License.
 */
 require("./style.css");
