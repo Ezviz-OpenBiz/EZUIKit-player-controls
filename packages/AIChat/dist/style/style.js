add0c127096c9db8e8b224c5c3e1c161aa0250b4
wangwenwen <48354328+wangwenwen130@users.noreply.github.com>
2026-06-05 11:22:54 +0800

    [fixed] 修复ios 浏览器 ui样式问题

--- packages/AIChat/dist/style/style.js
+++ packages/AIChat/dist/style/style.js
@@ -1,6 +1,6 @@
 /*
-* @ezuikit/control-aichat v0.1.0-beta.8
-* Copyright (c) 2026-06-03 Ezviz-OpenBiz
+* @ezuikit/control-aichat v0.1.0-beta.9
+* Copyright (c) 2026-06-05 Ezviz-OpenBiz
 * Released under the MIT License.
 */
 require("./style.css");
