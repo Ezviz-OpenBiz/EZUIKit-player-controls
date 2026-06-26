138b934a148bf1a2c002d3f0581f598ac33efcd0
ShineShao <xiaoshaoqq@gmail.com>
2026-06-26 14:23:01 +0800

    feat: call update hover tip

--- packages/TimeLine/dist/style/style.js
+++ packages/TimeLine/dist/style/style.js
@@ -1,6 +1,6 @@
 /*
-* @ezuikit/control-time-line v2.1.0-beta.2
-* Copyright (c) 2026-06-24 Ezviz-OpenBiz
+* @ezuikit/control-time-line v2.1.0-beta.3
+* Copyright (c) 2026-06-26 Ezviz-OpenBiz
 * Released under the MIT License.
 */
 require("./style.css");
