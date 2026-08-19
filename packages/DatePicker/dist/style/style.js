6992e35b793323ff6f96793742b94bbfde073f02
wangwenwen <48354328+wangwenwen130@users.noreply.github.com>
2026-08-19 16:02:01 +0800

    feat(date-picker): setCurrent sync current property to fix panel reset

--- packages/DatePicker/dist/style/style.js
+++ packages/DatePicker/dist/style/style.js
@@ -1,6 +1,6 @@
 /*
-* @ezuikit/control-date-picker v2.1.1
-* Copyright (c) 2026-08-03 09:22:56 Ezviz-OpenBiz
+* @ezuikit/control-date-picker v2.1.2-beta.1
+* Copyright (c) 2026-08-19 15:54:10 Ezviz-OpenBiz
 * Released under the MIT License.
 */
 require("./style.css");
