123d63c6054365fd7b3222f46dc191103d0f288c
ShineShao <xiaoshaoqq@gmail.com>
2026-09-15 12:47:00 +0800

    fix: Fixed list render

--- packages/RecListModal/dist/style/style.js
+++ packages/RecListModal/dist/style/style.js
@@ -1,6 +1,6 @@
 /*
-* @ezuikit/control-rec-list-modal v0.0.1
-* Copyright (c) 2026-08-03 09:25:39 Ezviz-OpenBiz
+* @ezuikit/control-rec-list-modal v0.0.2
+* Copyright (c) 2026-09-15 12:43:31 Ezviz-OpenBiz
 * Released under the MIT License.
 */
 require("./style.css");
