55f88ecb5bb2689bc50c432d868449a8124d470a
ShineShao <xiaoshaoqq@gmail.com>
2026-09-15 12:27:24 +0800

    fix: list render

--- packages/VirtualScroll/dist/style/style.js
+++ packages/VirtualScroll/dist/style/style.js
@@ -1,6 +1,6 @@
 /*
-* @ezuikit/control-virtual-scroll v0.0.1
-* Copyright (c) 2026-07-06 Ezviz-OpenBiz
+* @ezuikit/control-virtual-scroll v0.0.2
+* Copyright (c) 2026-09-15 12:22:14 Ezviz-OpenBiz
 * Released under the MIT License.
 */
 require("./style.css");
